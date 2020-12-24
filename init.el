b508faab8aafd831a780ce028cb2d34498ef5aee
wsd5656126 <wsd5656126@foxmail.com>
2020-12-24 19:31:23 +0800

    first commint

---
+++ init.el
@@ -0,0 +1,7 @@
+;;相信的心就是你的魔法
+;;引入子模块
+(package-initialize)
+(add-to-list 'load-path "~/.emacs.d/lisp")
+;;插件包管理
+(require 'init-packages)
+
---
+++ lisp/init-packages.el
@@ -0,0 +1,41 @@
+;; cl - Common Lisp Extension
+(require 'cl)
+
+;; Add Packages
+(defvar my/packages '(
+		      ;; --- Auto-completion ---
+		      company
+		      ;; --- Better Editor ---
+		      hungry-delete
+		      swiper
+		      counsel
+		      smartparens
+		      ;; --- Major Mode ---
+		      js2-mode
+		      ;; --- Minor Mode ---
+		      nodejs-repl
+		      exec-path-from-shell
+		      ;; --- Themes ---
+		      monokai-theme
+		      ;; solarized-theme
+		      ) "Default packages")
+
+(setq package-selected-packages my/packages)
+
+(defun my/packages-installed-p ()
+  (loop for pkg in my/packages
+	when (not (package-installed-p pkg)) do (return nil)
+	finally (return t)))
+
+(unless (my/packages-installed-p)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (dolist (pkg my/packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
+
+;; Find Executable Path on OS X
+(when (memq window-system '(mac ns))
+     (exec-path-from-shell-initialize))
+;;导出名称
+(provide 'init-packages)
