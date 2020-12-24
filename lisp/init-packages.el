d7a02cf8f39213465a91a913b10fe4f91cc79ac7
root <root@LAPTOP-AULNGI32.localdomain>
2020-12-24 23:17:45 +0800

    use tsinghua mirror

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -1,3 +1,8 @@
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+			         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/"))))
 ;; cl - Common Lisp Extension
 (require 'cl)
 
